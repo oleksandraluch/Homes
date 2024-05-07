8d365626bf47400242137e7eed275d1fe005a002
oleksandraluch <oluchakova@myseneca.ca>
2024-05-07 16:25:58 -0400

    HomeComponent works

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component'
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: `<h1>Hello World!</h1>`, // in-line template, wow
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent]
+})
+export class AppComponent {
+  title = 'Homes';
+}
