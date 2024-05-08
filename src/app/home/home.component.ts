6b2da3652071c98ab535247d4800e8b7d5bb9934
oleksandraluch <oluchakova@myseneca.ca>
2024-05-08 13:08:22 -0400

    Did housing filtering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,11 +17,21 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
-
+  filteredLocationList: HousingLocation[] = [];
+  
   constructor(){
     this.housingService.getAllHousingLocations()
     .then((housingLocationList: HousingLocation[]) => {
       this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
     })
+  }
+
+  filterResults(text: string){
+    if (!text) this.filteredLocationList = this.housingLocationList;
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
