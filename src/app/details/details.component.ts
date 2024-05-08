ab2deebe69f2b55ffb08738959afa6a1a5ea7c0a
oleksandraluch <oluchakova@myseneca.ca>
2024-05-08 11:39:32 -0400

    Added application form

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
@@ -8,7 +9,7 @@ import { HousingLocation } from '../housing-location';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
@@ -16,6 +17,11 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
 
   constructor(){
     // id is a local var
